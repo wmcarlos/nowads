0d1015d40294f5bc33d8eba94d5582c1cbc7e49e
= <=>
2017-01-29 13:06:50 -0430

    upload the login file with translate

--- views/view_login.php
+++ views/view_login.php
@@ -3,73 +3,73 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <h4 class="page-head-line">Please Login To Enter </h4>
+                    <h4 class="page-head-line">Por Favor Ingresa tus Datos de Usuario </h4>
 
                 </div>
 
             </div>
             <div class="row">
                 <div class="col-md-6">
-                   <h4> Login with facebook <strong> / </strong>Google :</h4>
+                   <!--<h4> Login with facebook <strong> / </strong>Google :</h4>
                     <br />
                     <a href="index.php?v=login" class="btn btn-social btn-facebook">
                             <i class="fa fa-facebook"></i>&nbsp; Facebook Account</a>
                     &nbsp;OR&nbsp;
                     <a href="index.php?v=login" class="btn btn-social btn-google">
                             <i class="fa fa-google-plus"></i>&nbsp; Google Account</a>
-                    <hr />
+                    <hr />-->
                     <form name="flogin" id="flogin" method="post" action="?v=login" autocomplete="off">
-                         <h4> Or Login with <strong>Zontal Account  :</strong></h4>
+                         <h4> Iniciar Sesi&oacute;n en <strong>NowAds  :</strong></h4>
                         <br />
                             <div class="form-group">
-                                <label for="txtusername">Username : </label>
+                                <label for="txtusername">Usuario : </label>
                                 <input type="text" name="txtusername" id="txtusername" class="form-control" />
                             </div>
                             <div class="form-group">
-                                <label for="txtpassword">Password :  </label>
+                                <label for="txtpassword"> Contrase&ntilde;a:  </label>
                                 <input type="password" name="txtpassword" id="txtpassword" class="form-control" />
                             </div>
                             <input type="hidden" name="operation" id="operation" value="login">
                             <hr />
-                            <button type="submit" class="btn btn-info"><span class="glyphicon glyphicon-user"></span> &nbsp;Log Me In </button>&nbsp;
+                            <button type="submit" class="btn btn-info"><span class="glyphicon glyphicon-user"></span> &nbsp;Iniciar Sesi&oacute;n </button>&nbsp;
                         </form>
                 </div>
                 <div class="col-md-6">
                     <div class="alert alert-info">
-                        This is a free bootstrap admin template with basic pages you need to craft your project. 
-                        Use this template for free to use for personal and commercial use.
+                        NowAds es una plataforma que te permitira mejorar los ingresos de tu web si usas
+                        programas como Adsense o Adnow ya que se enfoca en las siguientes caracteristicas
                         <br />
-                         <strong> Some of its features are given below :</strong>
+                         <strong> Caracteristicas de NowAds :</strong>
                         <ul>
                             <li>
-                                Responsive Design Framework Used
+                                Registrar tus sitios web y trabajar de forma individual
                             </li>
                             <li>
-                                Easy to use and customize
+                                Llevar el control de clicks en cada una de dichas web
                             </li>
                             <li>
-                                Font awesome icons included
+                                Controlar los codigos ocultos ya sea para activarlos o desactivarlos
                             </li>
                             <li>
-                                Clean and light code used.
+                                Integraci&oacute;n con otros c&oacute;digos ocultos
                             </li>
                         </ul>
                        
                     </div>
                     <div class="alert alert-success">
-                         <strong> Instructions To Use:</strong>
+                         <strong> Instrucciones de Uso:</strong>
                         <ul>
                             <li>
-                               Lorem ipsum dolor sit amet ipsum dolor sit ame
+                               Ingresa tus datos correctament
                             </li>
                             <li>
-                                 Aamet ipsum dolor sit ame
+                                 Verifica que tus web esten bien Configuradas
                             </li>
                             <li>
-                               Lorem ipsum dolor sit amet ipsum dolor
+                                Coloca los Codigos en tus Web tal cual el Estandar
                             </li>
                             <li>
-                                 Cpsum dolor sit ame
+                                 Genera ingresos en tus Webs
                             </li>
                         </ul>
                        
